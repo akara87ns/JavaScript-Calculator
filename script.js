bda68f84d7bdd295e28504753a56d33e97e956c8
Aleksandar Tadic <aleksandartadic.ns@gmail.com>
2018-04-23 20:17:58 +0200

    Delete button enabled

--- script.js
+++ script.js
@@ -7,23 +7,12 @@ window.onload = function() {
 let buttons = document.querySelectorAll("button");
 console.log(buttons);
 
-// let calcArray = [];
 for (let i = 0; i < buttons.length; i++) {
     buttons[i].addEventListener("click", function(e) {
         results.value += this.value;
-        // console.log(results.value.length);
-        // console.log(results.value[results.value.length - 1]);
-
-
-        // let calcArray = [];
-        // calcArray.push(this.value);
-        // console.log(calcArray);
 
         let lastChar = results.value.substr((results.value.length-1), 1);
         let oneBeforeLastChar = results.value.substr((results.value.length-2), 1);
-
-        console.log("last number: " + lastChar);
-        console.log("one before last number: " +oneBeforeLastChar);
 
         if (lastChar === oneBeforeLastChar) {
             if (lastChar === "+") {
@@ -40,37 +29,20 @@ for (let i = 0; i < buttons.length; i++) {
             else if (lastChar === ".") {
                 results.value = results.value.slice(0,-1);
             }
-            // console.log("same input operation");
-            // e.target.disabled = false;
-            // results.value--;
         }
-        
-
-        // if (lastCharacter === oneBeforeLastCharacter) {
-        //     if (lastCharacter === "+"  ||
-        //         lastCharacter === "-"  ||
-        //         lastCharacter === "*"  ||
-        //         lastCharacter === "/"  ||
-        //         lastCharacter === ".") {
-        //             e.preventDefault();
-        //         }
-        // }
     });
 }
 
 document.getElementById("clear").addEventListener("click", function() {
     results.value = "";
-    // calcArray = [];
+});
+
+document.getElementById("delete").addEventListener("click", function() {
+    results.value = results.value.slice(0, -1);
 });
 
 document.getElementById("equals").addEventListener("click", function() {
     let equalsOperation = eval(results.value);
     results.value = equalsOperation;
-    // console.log(results.value);
-
-    // calcArray.join();
-    // console.log(calcArray);
 });
 
-//Preventing input of some characters more than once
-// console.log(results.value);
