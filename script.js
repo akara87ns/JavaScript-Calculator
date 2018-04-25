38bb6e7fbaf0f8dc5c0509fe025f62d6c25d5e2f
Aleksandar Tadic <aleksandartadic.ns@gmail.com>
2018-04-25 14:07:00 +0200

    Calculator functionality improved

--- script.js
+++ script.js
@@ -8,7 +8,7 @@ let buttons = document.querySelectorAll("button");
 console.log(buttons);
 
 for (let i = 0; i < buttons.length; i++) {
-    buttons[i].addEventListener("click", function(e) {
+    buttons[i].addEventListener("click", function() {
         results.value += this.value;
 
         let lastChar = results.value.substr((results.value.length-1), 1);
@@ -30,6 +30,20 @@ for (let i = 0; i < buttons.length; i++) {
                 results.value = results.value.slice(0,-1);
             }
         }
+        if (equalRes === true) {
+            console.log("Now it is true");
+            let lastNum = lastChar;
+            console.log(lastNum);
+            if (lastNum >= 0 && lastNum <= 9) {
+                results.value = lastNum;
+                equalRes = false;
+                return equalRes;
+            } else if (lastNum === "+" || lastNum === "-" || lastNum === "*" || lastNum === "/" || lastNum === ".") {
+                // results.value += lastNum;
+                equalRes = false;
+                return equalRes;
+            }
+        }
     });
 }
 
@@ -41,8 +55,41 @@ document.getElementById("delete").addEventListener("click", function() {
     results.value = results.value.slice(0, -1);
 });
 
+// let equal = false;
+let equalRes = false;
 document.getElementById("equals").addEventListener("click", function() {
     let equalsOperation = eval(results.value);
     results.value = equalsOperation;
+
+    equalRes = true;
+    console.log("Now it's false");
+    // console.log(results.value);
+    return equalRes;
+    
+    // console.log(equalsOperation);
+    
+
+    // if (equalsOperation != results.value) {
+    //     console.log("not equal");
+    // } else {
+    //     console.log("equal");
+    // }
+
+    // equal = true;
+    // return equal;
+
+    // let equals = true;
+
+    // if (equals === true && this.lastChar >= 0) {
+    //     console.log("not a number");
+    //     let equals = false;
+    //     console.log(equalsOperation);
+    // } else {
+    //     console.log("number");
+    //     let equals = false;
+    //     console.log(equalsOperation);
+    // }
 });
+// console.log(equal);
+
 
